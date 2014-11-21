22b33c7a163e1cc86339b3be15ebe7a31233d02f
Arsalan Shah <manactric@gmail.com>
2014-11-21 23:13:22 +0500

    Create ossn_com.php

---
+++ ossn_com.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+/**
+ * Initialize the Component
+ *
+ * @return void;
+ * @access private;
+ */
+function ossn_redirect_www() {
+  $url = ossn_site_url();
+  $parase = parse_url($url);
+  $host = str_replace('www.', '', $parase['host']);
+  if($_SERVER['HTTP_HOST'] ==  $host){
+	 header("HTTP/1.1 301 Moved Permanently"); 
+	 $url = rtrim($url, '/');
+	 $url = "{$parase['scheme']}://www.{$parase['host']}{$_SERVER['REQUEST_URI']}";
+	 header("Location: {$url}"); 
+  }	
+}
+ossn_register_callback('ossn', 'init', 'ossn_redirect_www');
